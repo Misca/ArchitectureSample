b28a0da60d99ba013c139e9a88167c244027341c
mihai.mecea <meceamihai@gmail.com>
2020-04-29 18:03:10 +0300

    Updated project

--- app/src/main/java/com/misca/todo/view/bindings/ViewBindings.java
+++ app/src/main/java/com/misca/todo/view/bindings/ViewBindings.java
@@ -3,6 +3,7 @@ package com.misca.todo.view.bindings;
 import android.view.View;
 
 import androidx.annotation.ColorRes;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.databinding.BindingAdapter;
 
@@ -11,10 +12,20 @@ import androidx.databinding.BindingAdapter;
  */
 public class ViewBindings {
 
-    @BindingAdapter({"backgroundColor"})
-    public static void addFeedItems(View view, @ColorRes int colorRes) {
+    @BindingAdapter("backgroundColor")
+    public static void backgroundColor(View view, @ColorRes int colorRes) {
         int color = ContextCompat.getColor(view.getContext(), colorRes);
         view.setBackgroundColor(color);
+    }
+
+    @BindingAdapter("isVisible")
+    public static void setViewVisibleOrGone(@Nullable View view, boolean isVisible) {
+        if (view != null) {
+            int visibility = isVisible ? View.VISIBLE : View.GONE;
+            if (view.getVisibility() != visibility) {
+                view.setVisibility(visibility);
+            }
+        }
     }
 
 }
